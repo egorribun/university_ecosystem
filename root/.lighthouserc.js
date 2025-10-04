ed8f220d68fd18750c74ca37fa9cffc43a329688
Egor <egorribun2005@gmail.com>
2025-10-04 16:37:50 +0300

    chore(lighthouse): make configs esm-friendly

--- root/.lighthouserc.js
+++ root/.lighthouserc.js
@@ -1,1 +1,6 @@
-module.exports = require('./frontend/.lighthouserc.js')
+import { ci as frontendCi } from "./frontend/.lighthouserc.js"
+
+const config = { ci: frontendCi }
+
+export const ci = config.ci
+export default config
