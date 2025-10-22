dd4543da7371d0c48c8fd8e9b4a291fe4479b9b3
Egor <egorribun2005@gmail.com>
2025-10-22 17:56:10 +0300

    Format hidden hint typography inline

--- root/frontend/src/components/DashboardStories.tsx
+++ root/frontend/src/components/DashboardStories.tsx
@@ -461,9 +461,7 @@ export default function DashboardStories({
                 </Box>
               )}
 
-              <Typography sx={{ ...visuallyHidden }}>
-                {t("stories.viewer.hints.auto")}
-              </Typography>
+              <Typography sx={{ ...visuallyHidden }}>{t("stories.viewer.hints.auto")}</Typography>
 
               <Stack
                 spacing={viewerStory.cta_url ? 2 : 1}
