09faa45387bd96243eedd6e104d0eae8b83d2063
Egor <egorribun2005@gmail.com>
2025-11-12 21:02:09 +0300

    Fix type exports for MUI color scheme shim

--- root/frontend/src/shims/muiUseColorScheme.ts
+++ root/frontend/src/shims/muiUseColorScheme.ts
@@ -1,3 +1,3 @@
 export { useColorScheme as default } from "@mui/material/styles"
-export type { SupportedColorScheme } from "@mui/material/styles/createThemeWithVars"
-export type { ColorSchemeContextValue } from "@mui/system"
+export type { SupportedColorScheme } from "@mui/material/styles"
+export type { ColorSchemeContextValue } from "@mui/system/cssVars"
