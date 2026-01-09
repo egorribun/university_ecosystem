b3c011f1503c04e1922b92d486f98454ce758685
Egor <egorribun2005@gmail.com>
2026-01-09 18:11:04 +0300

    style: fix formatting in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,8 +2,6 @@ import { useTranslation } from "react-i18next"
 import { AppProviders } from "./AppProviders"
 import { AppRoutes } from "./AppRoutes"
 
-
-
 /**
  * World-Class Modular App Registry
  */
